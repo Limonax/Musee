14ad13f023096fc90900fb5fa8c09e89b1fdd2db
jmagry <justine.magry@mmibordeaux.com>
2019-10-30 15:33:54 +0100

    tu saoul justine

--- script.js
+++ script.js
@@ -10,5 +10,80 @@ function move() {
       width++;
       elem.style.width = width + '%';
     }
-  }a
+  }
 }
+
+/* ---------------------
+                         SLIDER
+                                 ----------------------------------------------- */
+
+
+
+
+// INIT
+var slides = document.getElementsByClassName("slider");
+var sDisplay = document.getElementsByClassName("sd");
+var index = 0;
+update();
+
+function slider(i) {
+  index = i;
+
+  // C'est pour récupérer la position en X de l'élément qu'ont veux afficher
+  var posInit = slides[i].style.transform;
+  posInit = posInit.split('(');
+  posInit = posInit[1];
+  posInit = posInit.split('%');
+  posInit = posInit[0];
+  posInit = Number(posInit)
+
+
+  for (let j = 0; j < slides.length; j++) {
+    var pos = slides[j].style.transform;
+    pos = pos.split('(');
+    pos = pos[1];
+    pos = pos.split('%');
+    pos = pos[0];
+    pos = Number(pos)
+    slides[j].style.transform = "translateX(" + (pos - posInit) + "%)";
+  }
+
+  update();
+}
+
+function update(){
+  for (let j = 0; j < sDisplay.length; j++) {
+    if(j == index){
+      sDisplay[j].classList.add("spselect");
+    }
+    else{
+      sDisplay[j].classList.remove("spselect");
+    }
+  }
+}
+
+function next(){
+  if(index < 2){
+    index++
+  }
+  else{
+    index = 0;
+  }
+
+  slider(index);
+}
+
+function prev(){
+  if(index > 0){
+    index--;
+  }
+  else{
+    index = 2;
+  }
+
+  slider(index);
+}
+
+
+
+
